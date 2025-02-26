7b54cf1b3ec7f78c3dd47adfcf15da80c571fe9a
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2025-02-27 06:33:02 +0800

    Add tool to handle queries with URLs

--- app/agents/tools/tools.py
+++ app/agents/tools/tools.py
@@ -1,6 +1,8 @@
 from app.agents.tools.news.fetch_news import get_latest_specific_news_tool, search_latest_news_tool, get_latest_general_news_tool
+from app.agents.tools.news.url_based import url_based_qa_tool
 tools = [
     get_latest_specific_news_tool,
     search_latest_news_tool,
     get_latest_general_news_tool,
+    url_based_qa_tool,
 ]
